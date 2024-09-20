957aacdb86f59c224b15540806dde0df4708a0d8
嘉俊 杜 <3319313822@qq.com>
2024-09-20 13:21:29 +0800

    2024920A

--- Mylab/vector.cpp
+++ Mylab/vector.cpp
@@ -96,3 +96,39 @@ Rank Vector<T>::find(T const& e, Rank lo, Rank hi) const {
 	while ((lo < hi--) && (e != _elem[hi]));
 	return hi;
 }
+template <typename T>
+Rank Vector<T>::insert(Rank r, T const& e) {
+	expand();
+	for (int i = _size; i > r; i--) _elem[i] = _elem[i - 1];
+	_elem[r] = e; _size++;
+	return r;
+}
+template <typename T> int Vector<T>::remove(Rank lo, Rank hi) {
+	if (lo == hi) return 0;
+	while (hi < _size) _elem[lo++] = _elem[hi++];
+	_size = lo;
+	shrink();
+	return hi - lo;
+}
+template <typename T> int Vector<T>::remove(Rank r) {
+	T e = _elem[r];
+	remove(r, r + 1);
+	return e;
+}
+template <typename T> int Vector<T>::deduplicate() {
+	int oldSize = _size;
+	Rank i = 1;
+	while (i < _size)
+		(find(_elem[i], 0, i) < 0) ?
+		i++ : remove(i);
+	return oldSize - _size;
+}
+template <typename T> void Vector<T>::traverse(void (*visit) (T&))
+{
+	for (int i = 0; i < _size; i++) visit(_elem[i]);
+}
+template <typename T> template <typename VST>
+void Vector<T>::traverse(VST& visit)
+{
+	for (int i = 0; i < _size; i++) visit(_elem[i]);
+}
