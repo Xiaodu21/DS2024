c92dbb0a7d6bb1f94469ec531905c5161ef44d4b
嘉俊 杜 <3319313822@qq.com>
2024-09-24 13:21:19 +0800

    2024924A

--- Mylab/vector.cpp
+++ Mylab/vector.cpp
@@ -132,3 +132,33 @@ void Vector<T>::traverse(VST& visit)
 {
 	for (int i = 0; i < _size; i++) visit(_elem[i]);
 }
+template <typename T> struct Increase
+{
+	virtual void operateor() (T& e) { e++ }
+};
+
+template <typename T> void increase ( Vector <T> & V )
+{
+	V.traverse(Increase<T>());
+}
+template <typename T> int Vector<T>::diosordered() const {
+	int n = 0;
+	for (int i = 1; i < _size; i++)
+		if (_elem[i - 1] > _elem[i]) n++;
+	return n;
+}
+template <typename T> int Vector(T)::uniquify() {
+	int oldSize = _size; int i = 1;
+	while (i < _size)
+		_elem[i - 1] == _elem[i] ? remove(i) : i++;
+	return oldSize - _size;
+}
+template <typename T> int Vector<T>::uniquify() {
+	Rank i = 0, j = 0;
+	while (++j < _size)
+		if (_elem[i] != _elem[j])
+			_elem[++i] = _elem[j];
+	_size = ++i; shrink();
+	return j - i;
+}
+
