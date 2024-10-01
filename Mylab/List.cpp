2767341528da072eed3589895afdfee3bfff7861
嘉俊 杜 <3319313822@qq.com>
2024-10-01 14:21:48 +0800

    2024101A

--- Mylab/List.cpp
+++ Mylab/List.cpp
@@ -234,4 +234,30 @@ ListNodePosi(T) List<T>::selectMax(ListNodePosi(T) p, int n) {
 			max = cur;
 	return max;
 }
-//归并排序
+//归并排序  
+//有序列表的二路归并
+template <typename T>
+void List<T>::merge(ListNodePosi(T) & p, int n, List<T>& L, ListNodePosi(T) q, int m) {
+	ListNodePosi(T) pp = p->data;
+	while ( 0 < m )
+		if ( ( 0 < n ) && ( p->data <= q->data ) )
+		{	
+			if (q == (p = p->succ)) break;
+			n--;
+		}
+		else
+		{
+			insertB(p, L.remove((q = q->succ)->pred));
+			m--;
+		}
+	p = pp->succ;
+}
+//分治策略 列表的二路归并
+template <typename T>
+void List<T>::mergeSort(ListNodePosi(T)& p, int n) {
+	if (n < 2) return;
+	int m = n >> 1;
+	ListNodePosi(T) q = p; for (int i = 0; i < m; i++) q = q->succ;
+	mergeSort(p, m);mergeSort(q, n - m);
+	merge(p, m, *this, q, n - m);
+}
