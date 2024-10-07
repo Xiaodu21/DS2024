7920806f3d99825d4931327b73b5d2999e5ae327
嘉俊 杜 <3319313822@qq.com>
2024-10-07 18:27:30 +0800

    2024102B

---
+++ exp2/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
+// Created by lenovo on 2024/10/7.
+//
---
+++ exp3/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
+// Created by lenovo on 2024/10/7.
+////
+// Created by lenovo on 2024/10/7.
+//
